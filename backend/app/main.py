8038a91e0c1227b15038654700fdb7d77755f8be
Zang <realmonster2004@gmail.com>
2025-03-14 21:55:50 +0700

    update backend cors in main

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,10 +3,20 @@ from . import models
 from .database import engine, get_db
 from .routers import post, user, auth
 from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# Cấu hình CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Chấp nhận tất cả các origin (có thể thay bằng ["http://localhost:3000"] nếu chỉ cho phép React)
+    allow_credentials=True,
+    allow_methods=["*"],  # Cho phép tất cả các phương thức (GET, POST, PUT, DELETE, ...)
+    allow_headers=["*"],  # Cho phép tất cả các headers
+)
 
 app.include_router(post.router)
 app.include_router(user.router)
