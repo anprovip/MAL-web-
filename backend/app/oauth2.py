b3c8810d03324a5a6fa4f9651c8d36b35ed9cb08
anprovip <antit1616@gmail.com>
2025-03-15 23:21:37 +0700

    add script insert dataset to db

--- backend/app/oauth2.py
+++ backend/app/oauth2.py
@@ -5,9 +5,7 @@ from . import schemas, database, models
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from .config import settings
-#SECRET_KEY
-#ALgorithm
-#Expriation time
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/login')
 
 
